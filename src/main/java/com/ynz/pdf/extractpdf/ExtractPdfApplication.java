6979db4ba0c04aa19914a0ba33bd80c11e6822e9
yichunzhao <zhaoyichun@hotmail.com>
2020-12-12 23:43:16 +0100

    Initial commit

---
+++ src/main/java/com/ynz/pdf/extractpdf/ExtractPdfApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.pdf.extractpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractPdfApplication.class, args);
+	}
+
+}
