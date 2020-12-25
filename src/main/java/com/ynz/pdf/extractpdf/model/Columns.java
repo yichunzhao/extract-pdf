ff7a3d445aa5a50a0d0e65b2eb26fc8af78568dd
yichunzhao <zhaoyichun@hotmail.com>
2020-12-25 23:15:16 +0100

    moving Columns to the model pack

--- src/main/java/com/ynz/pdf/extractpdf/model/Columns.java
+++ src/main/java/com/ynz/pdf/extractpdf/model/Columns.java
@@ -1,4 +1,4 @@
-package com.ynz.pdf.extractpdf.parser.states;
+package com.ynz.pdf.extractpdf.model;
 
 public enum Columns {
     DATE, DIRECTION, TICKER, PRICE, LOW_PRICE, HIGH_PRICE, CLOSING_PRICE, LAST_PRICE
